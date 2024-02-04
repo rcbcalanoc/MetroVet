f99ec745e3eaabc202395cf20e77c3f5d27f23a2
Jared <redaustria0816@gmail.com>
2024-02-05 00:05:31 +1000

    Added GET API of Dogs

--- app/src/main/java/com/example/metrovet_frontend/retrofit/MetroVetAPI.java
+++ app/src/main/java/com/example/metrovet_frontend/retrofit/MetroVetAPI.java
@@ -2,14 +2,23 @@ package com.example.metrovet_frontend.retrofit;
 
 import com.example.metrovet_frontend.model.Dog;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.*;
 
 
 public interface MetroVetAPI {
 
+    // Add Dog
     @POST("/add-dog")
     Call <Dog> save(@Body Dog dog);
 
+    // Get Dog by ID
+    @GET("/dog/{id}")
+    Call <List<Dog>> getDog();
 
+    // Get all Dogs
+    @GET("/dogs")
+    Call<List<Dog>> getAllDogs();
 }
