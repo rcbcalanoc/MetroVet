500735d7d406548aef7dcf1d763942ec5fb3de6c
k-nitta-author <k.nitta.author@gmail.com>
2024-02-06 15:46:41 +0800

    added final changes

--- app/src/main/java/com/example/metrovet_frontend/retrofit/RetrofitService.java
+++ app/src/main/java/com/example/metrovet_frontend/retrofit/RetrofitService.java
@@ -15,7 +15,7 @@ public class RetrofitService {
 
     private void initializeRetrofit() {
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.10:8080") // May need to change depending on your IP address and port used
+                .baseUrl("http://10.0.2.2:8080") // May need to change depending on your IP address and port used
                 .addConverterFactory(GsonConverterFactory.create(new Gson()))
                 .build();
     }
